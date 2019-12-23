68a0d9d29c05f0c0ca77d215c00fe14e094a0a0c
Evgenii Semenov <semenov.evgeny.92@gmail.com>
2019-12-23 19:17:37 +0700

    Initial Commit

---
+++ DNNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DNNews
+//
+//  Created by Evgenii Semenov on 23.12.2019.
+//  Copyright © 2019 Evgenii Semenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
